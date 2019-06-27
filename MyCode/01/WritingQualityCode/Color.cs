f1104d0343d9e00fb9ea4838840c46b37b3c2f6b
Wagner <wagnerhsu@hotmail.com>
2019-06-27 20:54:22 +0800

    WritingQualityCode

---
+++ MyCode/01/WritingQualityCode/Color.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WritingQualityCode
+{
+    public enum Color
+    {
+        Red = 1,
+        Blue
+    }
+}
---
+++ MyCode/01/WritingQualityCode/README.md
@@ -0,0 +1,6 @@
+﻿## 2019-06-27
+
+- Live Static Code analysis
+```
+dotnet add package Microsoft.CodeQuality.Analyzers
+```
